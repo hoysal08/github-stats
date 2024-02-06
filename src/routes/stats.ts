4f5f28aac112a6cf7722dbd1925d8374c0cd6d9e
Sooraj Hoysal <surajhoysal817@gmail.com>
2024-02-06 22:44:55 +0530

    added logger for info

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import github from '../github';
+import logger from '../utils/logger';
 
 const router = express.Router();
 
 router.route('/:username').get(async (req, res) => {
     const username = req.params.username;
+    logger.info(`Fetching stats for ${username}`);
     const stats = await github.getStreakStats(username);
     res.status(200).json(stats);
 })
